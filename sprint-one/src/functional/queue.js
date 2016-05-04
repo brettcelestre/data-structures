56d35d398d62102836b8e2c41b6b17dc083d98ca
brettcelestre <bcelestre@gmail.com>
2016-05-04 15:56:51 -0700

    Completed functional-shared instantiation tests for stack.js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -14,25 +14,37 @@ var Queue = function(){
       // originally were. It will be a new position for the key/value pair, since we 
       // will be inserting a new key/value at index 0, and need to make room.
       for( var i = Object.keys(storage).length ; i > 0; i--){
-        // i in this case will be 
+        // i in this case will be one index higher than the key/value pair we're
+        // dealing with, when we use storage[i], it's creating a new index 1 step up
+        // from where our pair currently lives. We set this equal to the actual value
+        // , storage[i - 1], which lifts that value up an index.
         storage[i] = storage[i - 1]; 
       }
+      // After all of that, we store the new value at the zeroth index, 'Bottom'
       storage[0] = value;
     } else {
+      // If the length of storage is currently 0, that means we don't have anything,
+      // and we store the value at storage[0]
       storage[0] = value;
     }
   };
 
   someInstance.dequeue = function(){
+    // Declare var temp, set equal to the oldest key/value pair
     var temp = storage[Object.keys(storage).length - 1];
+    // delete will remove the oldest key/value pair from storage
     delete storage[Object.keys(storage).length - 1];
+    // return temp
     return temp;
   };
 
   someInstance.size = function(){
+    // Check if the length of storage is less than or equal to 0
     if ( Object.keys(storage).length <= 0 ){
+      // If true, that means storage is empty, return 0
       return 0
     } else {
+    // Otherwise, return the current length of storage
     return Object.keys(storage).length;
     }
   };
