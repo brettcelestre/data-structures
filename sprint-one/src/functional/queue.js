99767988c4d5238022ceab071a5fcc146b5821ae
brettcelestre <bcelestre@gmail.com>
2016-05-04 12:33:11 -0700

    Finished stack.js and queue.js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,34 @@ var Queue = function(){
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    // Check if the length of storage is greater than 0
+    if ( Object.keys(storage).length > 0 ){
+      // If true, iterate from the highest index + 1 of the storage object down to zero
+      // Moves all property/value pairs up one index position above where there
+      // originally were. It will be a new position for the key/value pair, since we 
+      // will be inserting a new key/value at index 0, and need to make room.
+      for( var i = Object.keys(storage).length ; i > 0; i--){
+        // i in this case will be 
+        storage[i] = storage[i - 1]; 
+      }
+      storage[0] = value;
+    } else {
+      storage[0] = value;
+    }
   };
 
   someInstance.dequeue = function(){
+    var temp = storage[Object.keys(storage).length - 1];
+    delete storage[Object.keys(storage).length - 1];
+    return temp;
   };
 
   someInstance.size = function(){
+    if ( Object.keys(storage).length <= 0 ){
+      return 0
+    } else {
+    return Object.keys(storage).length;
+    }
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,16 +2,28 @@ var Stack = function(){
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {},
+      // Declare var index, set to 0
+      index = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    // Create a new property in storage with index, but increment index, set equal to value
+    storage[index++] = value;
   };
 
   someInstance.pop = function(){
+    // If index true, ( above 0 )
+    if ( index ){
+      // Return the --index in storage, then decreases index by 1 during storage access, and then 
+      // decreases index by one after storage is accessed
+      return storage[--index];
+    }
   };
 
   someInstance.size = function(){
+    // Return the index
+    return index;
   };
 
   return someInstance;
