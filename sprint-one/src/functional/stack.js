be901db780476846deb1a0b9f6e443ab1ba6ceb3
Brett Celestre <bcelestre@gmail.com>
2016-05-05 08:38:42 -0700

    Completed functional pattern tests for stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,16 +2,24 @@ var Stack = function(){
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {},
+      // Declare index to keep track of current storage index
+      index = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    // increase
+    storage[index++] = value;
   };
 
   someInstance.pop = function(){
+    if ( index ){
+      return storage[--index];
+    }
   };
 
   someInstance.size = function(){
+    return index;
   };
 
   return someInstance;
