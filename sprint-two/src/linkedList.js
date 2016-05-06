b72064d41111a069c696c5ee4fb5ee4808c98d93
brettcelestre <bcelestre@gmail.com>
2016-05-06 11:26:52 -0700

    Completed linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,82 +2,68 @@ var LinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
-  //list.tail.index = 0;
 
   list.addToTail = function(value){
-    
-    // Recursion function to find and return the last object, so we can assign
-    // the list.tail
-    var lastNextNull = function( obj, property, newNext ){
-      // Stores our entire tail
-      var result = {
-
-      };
-      // Base case
-      if ( obj.property === null ){
-        result.value = newNext.value;
-        // Assigns the very last o
-        result.next = newNext;
-        return result;
-      }
-      // Loop through list.head.next to the deepest .next property until null
-      // then assign list.tail to that value
-      for ( var key in obj ){
-        // Checks that 
-        if ( obj[key] !== null && typeof obj[key] === 'object'){
-          //
-          // this .next property has a nested object to loop through
-          lastNextNull( result, property, newNext );
-        }
-      }
-    };
-
-
-    
     // First time doing anything
-    if ( list.head === null  && list.tail === null){
+    if (list.head === null  && list.tail === null){
       // Assigns list.head to the current value
       list.head = Node(value);
-      // Assins list.tail to the current value
+      // Assigns list.tail to the current value
       list.tail = Node(value);
-      // 
-      list.head.next = list.tail;
-
     // if something exists
-    } else {      
-
-
-      // Assigns only the value to list.tail
-      list.tail = Node(value);
-
-      // Assigns the entire tail to the current heads next property
-      list.head.next = lastNextNull(list.head.next, 'next', list.tail);      
+    } else {
+      // Store the new tail node in newTail
+      var newTail = Node(value);      
+      // Stores the entire list.head in obj to make our for in loop 'recursive'
+      var obj = list.head;
+      // Iterate through obj
+      for (var key in obj) {
+        // Check if the current obj is not null
+        if (obj.next !== null) {
+          // If its not, we reassign obj to the current obj.next value
+          obj = obj.next;
+        } else {
+          // otherwise, if the current obj.next value is null, assign the tail to that
+          // value
+          obj.next = newTail;
+        } 
+      }
+      // Assign the new tail, newTail, node to list.tail
+      list.tail = newTail;
     }
   };
 
   list.removeHead = function(){
-
     // Stores the current heads value
     var headValue = list.head.value;
-
-    //list.head = list.tail;
-    //list.head = Node.next;
-    
     // Reassigns the head to the entire tail
     list.head = list.head.next;
-
     // Returns the removed heads value
     return headValue;
-
-    //list.head = 
-
-    // store list.head.next
   };
 
   list.contains = function(target){
-
+    var containsFlag = false;
+    // Declare obj, set equal to list.head
+    var obj = list.head;
+    // Iterate through obj
+    for (var key in obj) {
+      // Check if the current obj.value equals the target
+      if (obj.value === target) {
+        // Update containsFlag to true
+        containsFlag = true;
+      } 
+      // Check if the current obj.next is is not equal to null
+      if (obj.next !== null) {
+        // Update the obj var to be the current obj.next's value
+        obj = obj.next;
+      } 
+    }
+    // Return containsFlag
+    return containsFlag;
   };
-
+  
+  // return the list
   return list;
 };
 
