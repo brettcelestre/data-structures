a9d9104a3314824fd0dd59f8092b1bf38a305f83
brettcelestre <bcelestre@gmail.com>
2016-05-06 17:33:12 -0700

    Progress on tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,26 +1,72 @@
 var Tree = function(value){
   var newTree = {};
-  newTree.value = value;
+  newTree.value = 0;
+  newTree.children = [];
 
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
-
-  // your code here
-  newTree.children = null;  // fix me
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  var val = {"value": value, "children": []};
 
+  // New
+  _.extend(val, treeMethods);
+
+  this.children.push(val);
 };
 
 treeMethods.contains = function(target){
+  var test = false;
+  var obj = this.children;
 
+  // for (var key in obj) {
+  //   if (obj[key].value === target) {
+  //     test = true;
+  //   } else {
+  //     obj = obj[key].value;
+  //   }
+  // }
+
+  var checkTarget = function(obj, target) {
+    if (Array.isArray(obj)) {
+      // Obj is an array
+      _.each(obj, function(element) {
+        
+      });
+    } else {
+      // Obj is an object
+      _.each(obj, function(element, key, list) {
+        if (element === target && key === 'value') {
+          test = true;
+        } 
+        if (element.children.length > 0) {
+          checkTarget(element.children, target);
+        }
+      });
+    }
+    
+  };
+
+  // console.log('Obj:', obj);
+
+  // for (var i = 0; i < obj.length; i++) {
+  //   console.log(obj);
+  //   // if (obj[i].value === target) {
+  //   //   test = true;
+  //   // }
+
+  //   // if (obj[i].children.length !== 0) {
+  //   //   obj = obj[i].children;
+  //   // }
+  // }
+
+  checkTarget(obj);
+
+  return test;
 };
 
 
